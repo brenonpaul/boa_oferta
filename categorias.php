8c58192ff1053fee026838454b8613d466e96418
Brenon Paul <bp.brenon@gmail.com>
2019-09-30 15:08:26 -0300

    estruturação da página categorias

--- categorias.php
+++ categorias.php
@@ -15,53 +15,18 @@
 				echo ("<div class='col-2 recentes mt-4 rounded border border-secondary'>
 							<div class='row'>");
 							?>
-							<img src="imagens/categorias/<?php echo $row_produto['foto_produto']?>" class='imgProd rounded border-secondary'>
+							<img src="imagens/categorias/<?php echo $row_produto['foto_categoria']?>" class='imgProd rounded border-secondary'>
 							<?php
 							echo ("</div>
 							<div class='row pl-2' pr-2>");
 							?>
-							<h6><strong> Produto:</strong> <?php echo utf8_encode($row_produto['nome_produto']); ?></h6>
+							<h6><strong> <?php echo utf8_encode($row_produto['nome_categoria']); ?></strong></h6>
 							<?php	
 							echo("</div>
 							<div class='row pl-2' pr-2>");
 
-							?>
-								<h6><strong> Preço:</strong> R$<?php echo $row_produto['preco']; ?> Kg</h6>
-							<?php
-							echo("</div>
-							<div class='row pl-2' pr-2>");
-							?>
-							<h6><strong> Mercado:</strong> 
-								<?php
-									$sql = "SELECT nome_mercado FROM mercados, produtos WHERE fk_id_mercado = id_mercado AND fk_id_mercado = $row_produto[fk_id_mercado];"; 
-									
-                   					$resultado = $conexao->query($sql) OR trigger_error($conexao->error, E_USER_ERROR);
-                 					$consulta = $resultado->fetch_object(); 
-
-                        			echo utf8_encode($consulta->nome_mercado);
-								?>
-							</h6>
-							<?php
-								
-							echo("</div>
-							<div class='row pl-2' pr-2>");
-							?>
-								<h6><strong> Postado por:</strong> 
-									<?php
-										$sql = "SELECT apelido FROM usuarios, produtos WHERE fk_cpf = cpf and fk_cpf = $row_produto[fk_cpf];"; 
-									
-                   					$resultado = $conexao->query($sql) OR trigger_error($conexao->error, E_USER_ERROR);
-                 					$consulta = $resultado->fetch_object(); 
-
-                        			echo utf8_encode($consulta->apelido);
-									?>
-								</h6>
-								<?php
-							echo("</div>
-							<div class='row pl-2' pr-2>");
-							?>
-								<h6><strong> Visto no dia:</strong> <?php echo $row_produto['data_visu']; ?></h6>
-							<?php
+							
+							
 						echo("</div>
 							</div>
 					<div class='col-1'></div>");
