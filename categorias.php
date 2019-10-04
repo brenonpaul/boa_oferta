77a0f9209712f36318c1f637f37e8bb76b592133
Brenon Paul <bp.brenon@gmail.com>
2019-10-04 15:14:27 -0300

    trabalhando na tela perfil de usuários

--- categorias.php
+++ categorias.php
@@ -9,29 +9,29 @@ require_once "class/conexao.php";
 		$result_produto = "SELECT * FROM categorias ORDER BY nome_categoria ASC";
 		$resultado_produto = mysqli_query($conexao, $result_produto);
 		while($row_produto = mysqli_fetch_assoc($resultado_produto)){
-	
+			
 			echo ("<div class='col-2 recentes mt-4 rounded border border-secondary'>
 				<div class='row'>");
 				?>
 				<a href="telaProdutos.php?cat=<?php echo $row_produto['id_categoria']?>">
-				<img src="imagens/categorias/<?php echo $row_produto['foto_categoria']?>" class='imgProd rounded border-secondary'>
-				<?php
-				echo ("</div>
-					<div class='row d-flex justify-content-center'>");
+					<img src="imagens/categorias/<?php echo $row_produto['foto_categoria']?>" class='imgProd rounded border-secondary'>
+					<?php
+					echo ("</div>
+						<div class='row d-flex justify-content-center'>");
+						?>
+						
+						<h6 class="text-body"><strong><?php echo utf8_encode($row_produto['nome_categoria']); ?></strong></h6></a>
+						
+						<?php	
+						echo("</div>
+							");							
+						echo("</div>
+							<div class='col-1'></div>");
+					}
 					?>
-				
-					<h6 class="text-body"><strong><?php echo utf8_encode($row_produto['nome_categoria']); ?></strong></h6></a>
-			
-					<?php	
-					echo("</div>
-						");							
-					echo("</div>
-						<div class='col-1'></div>");
-				}
-				?>
-			</div>
-		</div>	
+				</div>
+			</div>	
 
-		<?php
-		require_once "rodape.php";
-		?>
+			<?php
+			require_once "rodape.php";
+			?>
--- seuPerfil.php
+++ seuPerfil.php
@@ -6,56 +6,123 @@ session_start();
 
 <div class="container">
 	<h2 class="mt-4 text-center mb-3">Seu Perfil</h2>
-	 	
-	<div class="row d-flex justify-content-center">
 
+	<div class="row">
+		<div class="col-3">
 			<?php 
-            	$result_usuario = "SELECT * FROM usuarios where email = '$_SESSION[usuario]'";  
-            	$resultado_usuario = mysqli_query($conexao, $result_usuario);
+			$result_usuario = "SELECT * FROM usuarios where email = '$_SESSION[usuario]'";  
+			$resultado_usuario = mysqli_query($conexao, $result_usuario);
 
-            
 
-            	while($row_usuario = mysqli_fetch_assoc($resultado_usuario)){
-			?>
 
-			
-	
-				<img src="imagens/<?php echo $row_usuario['foto_usuario']?>" class='imgProd rounded border border-secondary'>
-</div>
-<div class="text-center">
+			while($row_usuario = mysqli_fetch_assoc($resultado_usuario)){
+				?>
 
-				<h6><strong>Apelido:</strong> <?php echo $row_usuario['apelido']; ?></h6>
-				<h6><strong>Nome:</strong> <?php echo $row_usuario['nome_completo']; ?></h6>
-				<h6><strong>CPF:</strong> <?php echo $row_usuario['cpf']; ?></h6>
-				<h6><strong>E-mail:</strong> <?php echo $row_usuario['email']; ?></h6>
-				<h6><strong>Endereço:</strong> <?php 
-				$end = "SELECT nome_rua from ruas, usuarios WHERE id_rua = fk_id_rua_user and id_rua = '$row_usuario[fk_id_rua_user]'";
-			
-                   $usuario = $_SESSION['usuario'];
+
+
+				<img src="imagens/<?php echo $row_usuario['foto_usuario']?>" class="caixaPerfil" style="width: 260px">
+
+			</div>
+			<div class="col-1"></div>
+			<div class="col-3 caixaPerfil">
+				<h6 class="mt-4 mb-4"><strong>Apelido:</strong> <?php echo $row_usuario['apelido']; ?></h6>
+				<h6 class="mt-4 mb-4"><strong>Nome:</strong> <?php echo $row_usuario['nome_completo']; ?></h6>
+				<h6 class="mt-4 mb-4"><strong>CPF:</strong> <?php echo $row_usuario['cpf']; ?></h6>
+				<h6 class="mt-4 mb-4"><strong>E-mail:</strong> <?php echo $row_usuario['email']; ?></h6>
+			</div>
+			<div class="col-1"></div>
+			<div class="col-3 caixaPerfil">
+				<h6 class="mt-4 mb-4"><strong>Estado:</strong> <?php 
+
+				$usuario = $_SESSION['usuario'];
                    //$sql = "select nome_completo from usuarios where email = '$usuario' ;";
-                   $sql = "SELECT nome_rua from ruas, usuarios WHERE id_rua = fk_id_rua_user and id_rua = '$row_usuario[fk_id_rua_user]' and apelido = '$row_usuario[apelido]'";
-                   $resultado = $conexao->query($sql) OR trigger_error($conexao->error, E_USER_ERROR);
-                   while($consulta = $resultado->fetch_object()){
-                                   
-                        echo utf8_encode($consulta->nome_rua);
-                     
-                }
-             
+				$sql = "SELECT nome_estado FROM estados, cidades, bairros, ruas, usuarios WHERE id_estado = fk_id_estado AND id_cidade = fk_id_bairro AND id_bairro = fk_id_bairro AND id_rua = fk_id_rua_user AND id_rua = '$row_usuario[fk_id_rua_user]' AND apelido = '$row_usuario[apelido]'";
+				$resultado = $conexao->query($sql) OR trigger_error($conexao->error, E_USER_ERROR);
+				while($consulta = $resultado->fetch_object()){
+
+					echo utf8_encode($consulta->nome_estado);
+
+				}
+
 				?>
 
 			</h6>
-			<?php
-			}	  
-            ?>
 
-     </div>
-
-		
-	</div>
-	
+			<h6 class="mt-4 mb-4"><strong>Cidade:</strong> <?php 
 			
+			$usuario = $_SESSION['usuario'];
+                   //$sql = "select nome_completo from usuarios where email = '$usuario' ;";
+			$sql = "SELECT nome_cidade from  cidades, bairros, ruas, usuarios where id_cidade = fk_id_cidade and id_bairro = fk_id_bairro and id_rua = fk_id_rua_user and id_rua = '$row_usuario[fk_id_rua_user]' AND apelido = '$row_usuario[apelido]'";
+			$resultado = $conexao->query($sql) OR trigger_error($conexao->error, E_USER_ERROR);
+			while($consulta = $resultado->fetch_object()){
+
+				echo utf8_encode($consulta->nome_cidade);
+
+			}
+
+			?>
+
+		</h6>
+
+		<h6 class="mt-4 mb-4"><strong>Bairro:</strong> <?php 
+
+		$usuario = $_SESSION['usuario'];
+                   //$sql = "select nome_completo from usuarios where email = '$usuario' ;";
+		$sql = "SELECT nome_bairro from bairros, ruas, usuarios where id_bairro = fk_id_bairro and id_rua = fk_id_rua_user and id_rua = '$row_usuario[fk_id_rua_user]' AND apelido = '$row_usuario[apelido]'";
+		$resultado = $conexao->query($sql) OR trigger_error($conexao->error, E_USER_ERROR);
+		while($consulta = $resultado->fetch_object()){
+
+			echo utf8_encode($consulta->nome_bairro);
+
+		}
+
+		?>
+
+	</h6>
+
+	<h6 class="mt-4 mb-4"><strong>Sua rua:</strong> <?php 			
+	$usuario = $_SESSION['usuario'];
+                   //$sql = "select nome_completo from usuarios where email = '$usuario' ;";
+	$sql = "SELECT nome_rua from ruas, usuarios WHERE id_rua = fk_id_rua_user and id_rua = '$row_usuario[fk_id_rua_user]' and apelido = '$row_usuario[apelido]'";
+	$resultado = $conexao->query($sql) OR trigger_error($conexao->error, E_USER_ERROR);
+	while($consulta = $resultado->fetch_object()){
+
+		echo utf8_encode($consulta->nome_rua);
+
+	}
+
+	?>
+
+</h6>
+
+<?php
+}	  
+?>
+</div>
+
+
+
+
+</div>
+
+
+
+<?php
+	
+
+
+?>
+
+
+
+
+
+
+
+</div>
+
 </div>
 
 <?php
 require_once("rodape.php");
-?>
+	?>
